1d666a09d98de972db969b19de0cbac55ae01598
Variu <vavariu@wpi.edu>
2023-07-26 16:03:09 -0400

    wed work, 2nd part

---
+++ mywebsite/game/numgame.js
@@ -0,0 +1,27 @@
+const guess = document.getElementById("guess");
+const report = document.getElementById("report");
+
+const MAXNUM = 100;
+let secret;
+
+function loadGame() {
+    guess.max = MAXNUM;
+    secret = Math.floor(Math.random() * (MAXNUM + 1));
+    // report.innerHTML = secret;
+}
+
+function makeGuess() {
+    let myGuess = guess.value;
+
+    if (myGuess < secret) {
+        report.innerHTML += `<br/>[${myGuess}] HIGHER`;
+    }
+    else if (myGuess > secret) {
+        report.innerHTML += `<br/>[${myGuess}] LOWER`;
+    }
+    else {
+        report.innerHTML += `<br/>[${myGuess}] CORRECT`;
+        report.style.color = "green";
+        report.style.fontSize = "30px";
+    }
+}
